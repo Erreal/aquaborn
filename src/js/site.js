71d19e372de3ef879a058f3cc5ae9022e6ac93cb
Erreal <erreal@yandex.ru>
2020-04-03 19:21:05 +0300

    finished work on 03.04.20. Allmost done!)

---
+++ src/js/site.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            620:{
+                items:2
+            }
+            1000:{
+                items:3
+            }
+        }
+    });
+  });
